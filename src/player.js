909d5ec32033e4e39cc161078aa5089b2d63a17d
LEECH-SURFACE\liamd <liamdidato@gmail.com>
2017-08-12 16:21:41 +1000

    Level Ups

--- src/player.js
+++ src/player.js
@@ -1,6 +1,8 @@
 var weaponz = require('./weaponz.js');
 
 function Player(wep) {
+	this.maxHP = 25;
+	this.currHP = 25;
 	this.level = 1;
 	this.xp = 0;
 	this.str = 0;
@@ -10,8 +12,33 @@ function Player(wep) {
 	this.mdef = 0;
 	this.roomsEntered = 1;
 	this.eWeapon = wep;
-	function levelUpPlayer() {
-		
+	this.levelUpPlayer = function () {
+		// Randomly increases Str, Dex or Agi, and increases HP by a small amount.
+		var r = Math.floor(Math.random()*3);
+		switch (r) {
+			case 0:
+				this.str++;
+				break;
+			case 1:
+				this.dex++;
+				break;
+			case 2:
+				this.agi++;
+				break;
+		}
+		var inchp = Math.floor(Math.random()*3)+3;
+		this.maxHP += inchp;
+		this.level++;
+	}
+	this.checkLevel = function () { // Fuckin recursion mate
+		if (exports.xpForLevel(this.level+1) <= this.xp) {
+			this.levelUpPlayer();
+			this.checkLevel();
+		}
+	}
+	this.gainExp = function (amount) {
+		this.xp += amount;
+		this.checkLevel();
 	}
 }
 
@@ -38,4 +65,12 @@ exports.calcDifficulty = function (x, y, n) {
 
 // Test
 var pl = exports.createPlayer();
+console.log(JSON.stringify(pl));
+
+console.log(JSON.stringify(pl));
+pl.gainExp(4);
+console.log(JSON.stringify(pl));
+pl.gainExp(2);
+console.log(JSON.stringify(pl));
+pl.gainExp(22);
 console.log(JSON.stringify(pl));
