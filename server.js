b880d733da991c6e347c6397e4d522ef584cb3f4
Michael Devenish <michael@michaeldevenish.com>
2017-08-12 13:39:38 +1000

    added styling

--- server.js
+++ server.js
@@ -11,58 +11,48 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+app.use(express.static(__dirname + '/src'));
+app.set('views', __dirname+'/src');
+app.set('view engine', 'ejs');
 var idgen1 = new FlakeId();
 app.get('/', (req, res) => {
         //testing if files have been added to the docker correctly
         var contents = fs.readFileSync('src/test.txt','utf8');
         //testing id generation
         var id = intformat(idgen1.next(),'hex');
-        res.send('Your unique API key is:' + id + ' Ctest:'+contents );
+       // res.send('Your unique API key is:' + id + ' Ctest:'+contents );
+        res.render('index',{id: id, contents: contents});
         //show a help page
-});
-
-app.get('/j/:tagId/:tag1/:tag2', function(req, res) {
-		//access the main game using ip/apikey?input=value e.g http://13.59.173.76/577469cf19400000/test/a
-        var a = req.params.tagId;
-        var b = req.params.tag1;
-        var c = req.params.tag2;
-        var obj = new Object();
-        obj.APIkey = a;
-        obj.input = b;
-        obj.command = c;
-        res.json(obj);
-});
-
-app.get('/j/:tagId/:tag1', function(req, res) {
-		//access the main game using ip/apikey?input=value e.g http://13.59.173.76/577469cf19400000/test
-        var a = req.params.tagId;
-        var b = req.params.tag1;
-        var obj = new Object();
-        obj.APIkey = a;
-        obj.input = b;
-        res.json(obj);
 });
 
 app.get('/:tagId/:tag1/:tag2', function(req, res) {
 		//access the main game using ip/apikey?input=value e.g http://13.59.173.76/577469cf19400000/test/a
         var a = req.params.tagId;
-        var b = req.params.tag1;
-        var c = req.params.tag2;
-        if(b == undefined){
-                res.send("please enter a query");
+        var items = [req.params.tag1,req.params.tag2];
+        var json = req.query.json;
+        if(json != 'true'){
+            res.render('else',{id: a, contents: items});
         }else{
-                res.send('The input you provided is: ' + b + '\nUsing the key: ' + a + '\nand command: ' + c);
+        	var obj = new Object();
+	        obj.APIkey = a;
+	        obj.input = b;
+	        obj.command = c;
+	        res.json(obj);
         }
 });
 
 app.get('/:tagId/:tag1', function(req, res) {
 		//access the main game using ip/apikey?input=value e.g http://13.59.173.76/577469cf19400000/test
         var a = req.params.tagId;
-        var b = req.params.tag1;
-        if(b == undefined){
-                res.send("please enter a query");
+        var b = [req.params.tag1];
+        var json = req.query.json;
+        if(json != 'true'){
+            res.render('else',{id: a, contents: b});
         }else{
-                res.send('The input you provided is: ' + b + '\nUsing the key: ' + a);
+            var obj = new Object();
+	        obj.APIkey = a;
+	        obj.input = b;
+	        res.json(obj);
         }
 });
 
