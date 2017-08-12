cadcaff5a1e1129522c4fac4e0f21d3f9e136cc0
Michael Devenish <michael@michaeldevenish.com>
2017-08-12 12:30:57 +1000

    worked on url patterns

--- server.js
+++ server.js
@@ -21,10 +21,44 @@ app.get('/', (req, res) => {
         //show a help page
 });
 
-app.get('/:tagId', function(req, res) {
-		//access the main game using ip/apikey?input=value e.g http://13.59.173.76/577469cf19400000?input=test
+app.get('/j/:tagId/:tag1/:tag2', function(req, res) {
+		//access the main game using ip/apikey?input=value e.g http://13.59.173.76/577469cf19400000/test/a
         var a = req.params.tagId;
-        var b = req.query.input;
+        var b = req.params.tag1;
+        var c = req.params.tag2;
+        var obj = new Object();
+        obj.APIkey = a;
+        obj.input = b;
+        obj.command = c;
+        res.json(obj);
+});
+
+app.get('/j/:tagId/:tag1', function(req, res) {
+		//access the main game using ip/apikey?input=value e.g http://13.59.173.76/577469cf19400000/test
+        var a = req.params.tagId;
+        var b = req.params.tag1;
+        var obj = new Object();
+        obj.APIkey = a;
+        obj.input = b;
+        res.json(obj);
+});
+
+app.get('/:tagId/:tag1/:tag2', function(req, res) {
+		//access the main game using ip/apikey?input=value e.g http://13.59.173.76/577469cf19400000/test/a
+        var a = req.params.tagId;
+        var b = req.params.tag1;
+        var c = req.params.tag2;
+        if(b == undefined){
+                res.send("please enter a query");
+        }else{
+                res.send('The input you provided is: ' + b + '\nUsing the key: ' + a + '\nand command: ' + c);
+        }
+});
+
+app.get('/:tagId/:tag1', function(req, res) {
+		//access the main game using ip/apikey?input=value e.g http://13.59.173.76/577469cf19400000/test
+        var a = req.params.tagId;
+        var b = req.params.tag1;
         if(b == undefined){
                 res.send("please enter a query");
         }else{
